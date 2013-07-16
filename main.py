dd8dd692f25703c6465a4d4936468533565ae07f
LittleQ <littleq0903@gmail.com>
2013-07-16 17:16:09 +0800

    added email demo

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Licensed by "Do whatever you want License" 1.0
 import bottle
 
+from google.appengine.api import mail
+
 DEBUG = True
 
 @bottle.route('/')
@@ -11,6 +13,20 @@ def helloworld():
             'greeting': 'Hello World'
             }
     return data
+
+
+@bottle.route('/maildemo')
+def sendmail():
+    SENDER = 'littleq0903@gmail.com'
+    RECIPIANT = 'colin@noderabbit.com'
+    SUBJECT = 'Hello from Taipei.py'
+    CONTENT = """
+    <h1>Hello</h1>
+    <p>Taipei.py Google Cloud Workshop</p>
+    """
+    
+    mail.send_mail(sender=SENDER, to=RECIPIANT, subject=SUBJECT, body=CONTENT)
+    return 'ok'
 
 
 app = bottle.default_app()
